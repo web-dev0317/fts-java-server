0ac58123e580b2e389d36127fbe6862ee0715bf6
HR <aspiringwebdeveloper0317@gmail.com>
2022-02-08 19:12:58 +0530

    using System.getenv().get()

--- src/main/java/io/berbotworks/mc/security/filters/SecurityConstants.java
+++ src/main/java/io/berbotworks/mc/security/filters/SecurityConstants.java
@@ -1,21 +1,6 @@
 package io.berbotworks.mc.security.filters;
 
-import java.util.Map;
-
 public interface SecurityConstants {
-    String JWT_KEY = Key.getKey();
+    String JWT_KEY = System.getenv().get("JWT_KEY");
     String JWT_HEADER = "Authorization";
-}
-
-class Key {
-    static String getKey() {
-        Map<String, String> env = System.getenv();
-        String key = null;
-        for (String envName : env.keySet()) {
-            if (envName.equalsIgnoreCase("jwt_key")) {
-                key = env.get(envName);
-            }
-        }
-        return key;
-    }
 }
