d7ffb758c57e4901f1edde7e20deac511ce2dcba
HR <aspiringwebdeveloper0317@gmail.com>
2022-02-08 19:23:21 +0530

    hardcoded key

--- src/main/java/io/berbotworks/mc/security/filters/SecurityConstants.java
+++ src/main/java/io/berbotworks/mc/security/filters/SecurityConstants.java
@@ -1,6 +1,6 @@
 package io.berbotworks.mc.security.filters;
 
 public interface SecurityConstants {
-    String JWT_KEY = System.getenv().get("JWT_KEY");
+    String JWT_KEY = "bbOtj4va1saUu3sOm3k1ngb4dmRr0_r345Jnek3n-1";
     String JWT_HEADER = "Authorization";
 }
