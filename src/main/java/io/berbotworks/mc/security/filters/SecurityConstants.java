ea3b2972024f2acae4d72fcad6bc075714cab2f3
HR <aspiringwebdeveloper0317@gmail.com>
2022-02-08 18:43:40 +0530

    Changed the way of getting key from env

--- src/main/java/io/berbotworks/mc/security/filters/SecurityConstants.java
+++ src/main/java/io/berbotworks/mc/security/filters/SecurityConstants.java
@@ -1,6 +1,21 @@
 package io.berbotworks.mc.security.filters;
 
+import java.util.Map;
+
 public interface SecurityConstants {
-    String JWT_KEY = System.getenv("JWT_KEY");
+    String JWT_KEY = Key.getKey();
     String JWT_HEADER = "Authorization";
+}
+
+class Key {
+    static String getKey() {
+        Map<String, String> env = System.getenv();
+        String key = null;
+        for (String envName : env.keySet()) {
+            if (envName.equalsIgnoreCase("jwt_key")) {
+                key = env.get(envName);
+            }
+        }
+        return key;
+    }
 }
